2cb02ea5b3762b64f401778ab4d421f95e129939
Lucas Oliveira <lucas_7tv@hotmail.com>
2025-07-18 12:29:16 -0300

    feat: add functions to get user infos and tasks

---
+++ src/actions/DashBoardActions.ts
@@ -0,0 +1,30 @@
+import Cookies from 'js-cookie';
+
+
+export interface User {
+    id: number;
+    name: string;
+    email: string;
+    created_at: Date;
+    updated_at: Date;
+}
+
+export async function getUserTasks(email: string) {
+    const token = Cookies.get('token');
+    const response = await fetch(`http://localhost:8000/api/v1/get_tasks_by_user_email?email=${email}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+    });
+    const data = await response.json();
+    return data;
+}
+
+
+export function useGetUserSession() {
+    const session = Cookies.get('session');
+    const user = session ? JSON.parse(session) : null;
+    return user
+}
