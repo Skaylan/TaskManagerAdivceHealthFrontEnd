3b5321442371155f76fc83f9576f6a4f491d4bbb
Lucas Oliveira <lucas_7tv@hotmail.com>
2025-07-18 12:28:43 -0300

    Feat: create the dashboard page

--- src/pages/Dashborad.tsx
+++ src/pages/Dashborad.tsx
@@ -1,7 +1,93 @@
+// import { useGetUserInfo } from "@/actions/DashBoardActions";
+import { getUserTasks, useGetUserSession } from "@/actions/DashBoardActions";
+import { Header } from "@/components/Header";
+import { TaskCard } from "@/components/TaskCard";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { useQuery } from "@tanstack/react-query";
+// import Cookies from "js-cookie";
+import {
+    Plus,
+    Search,
+    Filter,
+  } from 'lucide-react';
+
+
+
+export interface Task {
+    id: number;
+    title: string;
+    description: string;
+    is_done: boolean;
+    created_at: string;
+    updated_at: string
+}
 export default function Dashboard() {
+
+    const { data: tasks } = useQuery({
+        queryKey: ['tasks'],
+        queryFn: () => getUserTasks(useGetUserSession().email)
+    });
+
+
+    // console.log(tasks)
+
     return (
-        <div>
-            <h1>Dashboard</h1>
+        <div className="w-full flex flex-col items-center">
+            <div className="w-1/2 flex flex-col">
+                <div className="flex w-full">
+                    <Header />
+                </div>
+                <div className="flex flex-col sm:flex-row gap-4 mb-6">
+                    <div className="flex-1 relative">
+                        <Search className="absolute left-3 top-3 h-4 w-4 text-muted-foreground" />
+                        <Input
+                            placeholder="Search tasks..."
+                            // value={searchTerm}
+                            // onChange={(e) => setSearchTerm(e.target.value)}
+                            className="pl-10"
+                        />
+                    </div>
+                    <div>
+                        <Select>
+                            <SelectTrigger className="w-[180px] cursor-pointer">
+                                <Filter className="h-4 w-4 mr-2" />
+                                <SelectValue placeholder="Filtros" />
+                            </SelectTrigger>
+                            <SelectContent className="">
+                                <SelectGroup className="">
+                                    <SelectLabel>Filtros</SelectLabel>
+                                    <SelectItem className="cursor-pointer" value="Todos">
+                                        Todos
+                                    </SelectItem>
+                                    <SelectItem className="cursor-pointer" value="done">
+                                        Concluídos
+                                    </SelectItem>
+                                    <SelectItem className="cursor-pointer" value="not-done">
+                                        Pendentes
+                                    </SelectItem>
+                                </SelectGroup>
+                            </SelectContent>
+                        </Select>
+                    </div>
+
+
+                    <Button
+
+                        // onClick={() => setIsTaskFormOpen(true)}
+                        className="cursor-pointer"
+                    >
+                        <Plus className="h-4 w-4 mr-2" />
+                        Add Task
+                    </Button>
+                </div>
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                    {tasks?.map((task: Task) => (
+                        <TaskCard key={task.id} task={task} />
+                    ))}
+                </div>
+            </div>
         </div>
     );
 }
