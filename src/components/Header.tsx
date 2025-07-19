ad893e777d0f146ba619e678cbf7990ee04244c9
Lucas Oliveira <lucas_7tv@hotmail.com>
2025-07-19 13:51:27 -0300

    Feat: add logout functionality

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,18 @@
 import { LogOut } from "lucide-react";
 import { Button } from "./ui/button";
-
+import Cookies from "js-cookie";
 export function Header() {
+
+    const logOut = () => {
+        Cookies.remove("token");
+        Cookies.remove("session");
+        window.location.href = "/";
+    }
+
     return (
         <header className="w-full flex p-8 justify-between">
             <h1 className="text-3xl font-bold">Minhas Tarefas</h1>
-            <Button variant={"outline"} className="ml-auto cursor-pointer"><LogOut/> Logout</Button>
+            <Button onClick={logOut} variant={"outline"} className="ml-auto cursor-pointer"><LogOut/>Sair</Button>
         </header>
     )
 }
